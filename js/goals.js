3d431b86312ffa18a6fc6482894b130c8f71b89e
alex-krasnoselskikh <johan.doe@bk.ru>
2017-12-08 19:36:51 +0300

    goals-form

--- js/goals.js
+++ js/goals.js
@@ -1,21 +1,24 @@
-//"accumulations-goals"
 fetch('/jsons/goals.json')
 .then(res => res.json())
 .then(data => {
   let out = "";
   data.forEach(element => {
     let progress = element.collected * 100 / element.price
+    let prWidth = progress
     let backgroundColor = "#4CAF50";
     let color = "white";
-    if(progress == 0) {
+    if (progress == 0) {
       color = "black";
       backgroundColor = "#ddd";
+    }
+    if (progress <= 15) {
+      prWidth = 15;
     }
     out += `<div>${element.name}: ${element.collected} / ${element.price} ${element.currency}
     <div id="${element.name}" style="width: 100%; background-color: #ddd;">
       <div id="${element.name}-progress" 
       style="
-        width: ${progress}%;
+        width: ${prWidth}%;
         box-sizing: content-box;
         height:1.5em;
         background-color: ${backgroundColor};
@@ -24,6 +27,7 @@ fetch('/jsons/goals.json')
         font-size: 1em;
         color: ${color}; 
         white-space: nowrap;
+        overflow: hidden;
         marign: 0;">
           ${progress}%
         </div>
@@ -33,7 +37,5 @@ fetch('/jsons/goals.json')
  
   });
   document.querySelector('.accumulations-goals').innerHTML = out;
-  // document.querySelector("#Телефон-progress").style.color = "black";
-  // document.querySelector("#Телефон-progress").style.backgroundColor = "";
 })
 .catch(err => console.log(err));
